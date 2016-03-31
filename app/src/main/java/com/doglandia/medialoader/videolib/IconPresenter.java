327dc8ce3ebe4b7efdd85baed0de2c3fa7498803
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2016-03-31 19:54:42 -0400

    mess around with ImageCardView

--- app/src/main/java/com/doglandia/medialoader/videolib/IconPresenter.java
+++ app/src/main/java/com/doglandia/medialoader/videolib/IconPresenter.java
@@ -4,9 +4,12 @@ import android.graphics.Color;
 import android.support.v17.leanback.widget.BaseCardView;
 import android.support.v17.leanback.widget.ImageCardView;
 import android.support.v17.leanback.widget.Presenter;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.doglandia.medialoader.R;
 
@@ -32,6 +35,15 @@ public class IconPresenter extends Presenter {
 
         cardView.setFocusable(true);
         cardView.setFocusableInTouchMode(true);
+
+        cardView.setMainImageDimensions(156,172);
+        ImageView imageView = (ImageView) cardView.findViewById(R.id.main_image);
+        imageView.setPadding(20,20,20,20);
+
+        TextView textView = (TextView) ((ViewGroup) cardView.findViewById(R.id.info_field)).getChildAt(0);
+        textView.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        textView.setGravity(Gravity.CENTER);
+
         return new Presenter.ViewHolder(cardView);
 //        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.icon_item,parent,false));
     }
@@ -41,14 +53,13 @@ public class IconPresenter extends Presenter {
         ActionIcon actionIcon = (ActionIcon) item;
 
         ImageCardView cardView = (ImageCardView) viewHolder.view;
-        cardView.setMainImageDimensions(120,140);
+
         cardView.setMainImageAdjustViewBounds(true);
         cardView.getMainImageView().setColorFilter(Color.WHITE);
 
 
-//        cardView.setTitleText(actionIcon.getTitle());
+        cardView.setTitleText(actionIcon.getTitle());
 
-        cardView.setContentText(actionIcon.getTitle());
         cardView.setOnClickListener(actionIcon.getOnClickListener());
         cardView.setMainImage(cardView.getResources().getDrawable(actionIcon.getResId()));
 
